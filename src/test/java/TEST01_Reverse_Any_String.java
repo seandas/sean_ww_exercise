33e37998abb387b3f4f8c97d3322996ab039c4ab
SDAS-PC\SDAS <seanxdas@gmail.com>
2017-02-05 01:18:07 -0500

    Edit

--- src/test/java/TEST01_Reverse_Any_String.java
+++ src/test/java/TEST01_Reverse_Any_String.java
@@ -5,11 +5,10 @@ import org.testng.annotations.Test;
  * Created by SDAS on 2/2/2017.
  */
 public class TEST01_Reverse_Any_String extends ReusableMethods {
-    public String s = "It's always a pleasure to meet someone friendly! I wish everyone were friendly!!!";
+    public String s = "It's always a pleasure to meet someone friendly!!!";
 
 
     @Test(priority = 1, description = "testReverseEverythingPos")
-
     public void displayReversedString() {
         Reporter.log("Actual String Value is: \t" + s, true);
         Reporter.log("Reversed String Value is: \t" + reverseEverything(s), true);
